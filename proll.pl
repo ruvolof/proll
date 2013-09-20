f88eed11807c909a47ebf9fc3cfe43bc6b2939dd
Francesco Ruvolo <ruvolof@gmail.com>
2013-09-20 21:57:18 +0200

    quotes changed

--- proll.pl
+++ proll.pl
@@ -25,9 +25,9 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-my $PROGNAME = "PRoll";
-my $VERSION = "1.0";
-my $AUTHOR = "Francesco Ruvolo <ruvolof\@gmail.com>";
+my $PROGNAME = 'PRoll';
+my $VERSION = '1.0';
+my $AUTHOR = 'Francesco Ruvolo <ruvolof@gmail.com>';
 
 my $version = sub {
 	print "$PROGNAME $VERSION - $AUTHOR\n";
