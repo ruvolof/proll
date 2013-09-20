5214f19cfe239defaf1a34afac48ee78c5a2febe
Francesco Ruvolo <ruvolof@gmail.com>
2013-09-20 14:14:23 +0200

    Fixed licensing date

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # 
 #       install.sh
 #       
-#       Copyright 2012 Francesco Ruvolo <ruvolof@gmail.com>
+#       Copyright 2012-2013 Francesco Ruvolo <ruvolof@gmail.com>
 #       
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
